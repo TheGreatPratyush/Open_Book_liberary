65d1eacb219afc9d51751b8fd543d17485332f31
The GreatPratyush <pratushgupta27199@gmail.com>
2026-04-01 11:01:27 +0530

    api Fertching

--- explore.js
+++ explore.js
@@ -1,15 +1,70 @@
-function card(aut,im,tit){
+let main = document.getElementById("main")
+
+
+
+
+
+function header(){
+    let outerDiv= document.createElement("div")
+    outerDiv.setAttribute("id","navi")
+
+    let innerDiv1 = document.createElement("div")
+    innerDiv1.innerHTML="BookFinder"
+    outerDiv.appendChild(innerDiv1)
 
     let p = document.createElement("div")
+    p.setAttribute("id","navi_sub")
+    p.innerHTML=`
+                <div class="navi_com">Home</div>
+                <div class="navi_com">Explore</div>
+                <div class="navi_com">Favourites</div>
+                <div class="navi_com">Dark</div>`
+    outerDiv.appendChild(p)
 
+    main.appendChild(outerDiv)
+    
+    
+}
+
+header()
+
+function searchBar(){
+    let p = document.createElement("div")
+    p.innerHTML=`
+    <div id="head">
+    <h2>Find Your Next Book</h2>
+    <h4>Search From thousands of  books instantly</h4>
+    </div>
+    <div id="searchBar">
+    <input type="text" placeholder="Search by title , author .." id="input">
+    <button id="searchBtn">Search</button>
+    </div>`
+    main.appendChild(p)
+}
+
+searchBar()
+
+let cards = document.getElementById("cards")
+cards.style.display="grid"
+cards.style.gap="20px"
+cards.style.gridTemplateColumns="repeat(4,1fr)"
+main.appendChild(cards)
+cards.style.marginTop="100px"
+
+
+
+function card(aut,im,tit){
+    
+    let p = document.createElement("div")
+    
     let img = document.createElement("img")
     img.src=`https://covers.openlibrary.org/b/id/${im}-M.jpg`
     img.style.height="auto"
     img.style.width="100%"
     img.style.objectFit="cover"
     p.appendChild(img)
-
-
+    
+    
     let book = document.createElement("p")
     book.innerHTML=tit
     p.appendChild(book)
@@ -28,17 +83,13 @@ function card(aut,im,tit){
 }
 
 
-
-let cards = document.getElementById("cards")
-cards.style.display="grid"
-cards.style.gap="20px"
-cards.style.gridTemplateColumns="repeat(4,1fr)"
-
-
-async function friction_books() {
-    let a = await fetch("https://openlibrary.org/search.json?q=fiction")
+async function friction_books(parame) {
+    let a = await fetch(`https://openlibrary.org/search.json?q=${parame}`)
     let p = await a.json()
     console.log(p)
+    if (p.docs.length==0){
+        alert("Please Input Valid Search")
+        return }
     let data = p.docs.map((e)=>{
         let p = {}
         p["author"]=e.author_name[0]
@@ -52,8 +103,19 @@ async function friction_books() {
     })
 }
 
+let input = document.getElementById("input")
+let searchBtn= document.getElementById("searchBtn")
+function searchBook(){
+    let value=input.value
+    if (value==""){
+        alert("Please input ")
+        return
+    }
+    cards.innerHTML=""
+    friction_books(value)
+}
+searchBtn.addEventListener("click",searchBook)
 
 
-
-friction_books()
+friction_books("fiction")
 
